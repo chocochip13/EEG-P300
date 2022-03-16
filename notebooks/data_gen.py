af13f14e3c15209830384df9bd8bebd3e70b0b84
chocochip13 <kapeleshh@gmail.com>
2022-03-17 02:07:24 +0900

    write epochs and labels as arrays

--- notebooks/data_gen.py
+++ notebooks/data_gen.py
@@ -35,4 +35,31 @@ for i in range(len(jdf)):
     ev.append(ep[i].average(by_event_type=True))
 
 for i in range(len(ep)):
-    ep[i].save('/workspace/data/EEG/data/epochs/A0' + str(i+1) + '-epo.fif')
+    ep[i].save('/workspace/data/EEG/data/epochs/A0' + str(i + 1) + '-epo.fif')
+
+#Label extraction
+l1 = ep[0].events[:,-1]
+l2 = ep[1].events[:,-1]
+l3 = ep[2].events[:,-1]
+l4 = ep[3].events[:,-1]
+l5 = ep[4].events[:,-1]
+l6 = ep[5].events[:,-1]
+l7 = ep[6].events[:,-1]
+l8 = ep[7].events[:,-1]
+
+X1 = ep[0].get_data()
+X2 = ep[1].get_data()
+X3 = ep[2].get_data()
+X4 = ep[3].get_data()
+X5 = ep[4].get_data()
+X6 = ep[5].get_data()
+X7 = ep[6].get_data()
+X8 = ep[7].get_data()
+
+#Combine epochs and labels
+combined_epochs = np.stack((X8, X7, X6, X5, X4, X3, X2, X1), axis=0)
+combined_labels = np.stack((l1,l2,l3,l4,l5,l6,l7,l8), axis=0)
+
+#Write as arrays
+np.save('/workspace/data/EEG/data/epochs/A-epo.npy', combined_epochs)
+np.save('/workspace/data/EEG/data/epochs/A-labels.npy', combined_labels)
